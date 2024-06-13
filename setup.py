d5eaf342dec0ff8cbac197bada56ba5649b3326f
mike <eugemike09@gmail.com>
2024-06-13 17:37:28 +0200

    Add Dockerfile and packaging files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='log_analyzer',
+    version='0.1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'log-analyzer=log_analyzer.cli:main',
+        ],
+    },
+    install_requires=[
+        'pandas',
+    ],
+)
